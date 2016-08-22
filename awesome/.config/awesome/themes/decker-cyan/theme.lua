277e345dcbc6668bd2148cbfc4c09084759729d1
pyratebeard <root@pyratebeard.net>
2016-08-22 23:03:58 +0100

    background change

--- awesome/.config/awesome/themes/decker-cyan/theme.lua
+++ awesome/.config/awesome/themes/decker-cyan/theme.lua
@@ -22,7 +22,7 @@ if not awful.util.file_readable(themes .. themename .. "/theme.lua") then
 end
 themedir = themes .. themename
 
-wallpaper1    = themedir .. "/wires.jpg"
+wallpaper1    = themedir .. "/background.jpg"
 wallpaper2    = themedir .. "/background.png"
 wallpaper3    = sharedthemes .. "/zenburn/zenburn-background.png"
 wallpaper4    = sharedthemes .. "/default/background.png"
