d5558f4be534e00efb240f7064576070d4bbd448
dudley <pyratebeard@gmail.com>
2016-09-09 16:49:58 +0100

    removed tasklist button configuration

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -138,43 +138,8 @@ mywibox = {}
 mypromptbox = {}
 mylayoutbox = {}
 mytaglist = {}
-
 mytasklist = {}
-mytasklist.buttons = awful.util.table.join(
-					 awful.button({ }, 1, function (c)
-											  if c == client.focus then
-												  c.minimized = true
-											  else
-												  -- Without this, the following
-												  -- :isvisible() makes no sense
-												  c.minimized = false
-												  if not c:isvisible() then
-													  awful.tag.viewonly(c:tags()[1])
-												  end
-												  -- This will also un-minimize
-												  -- the client, if needed
-												  client.focus = c
-												  c:raise()
-											  end
-										  end),
-					 awful.button({ }, 3, function ()
-											  if instance then
-												  instance:hide()
-												  instance = nil
-											  else
-												  instance = awful.menu.clients({
-													  theme = { width = 250 }
-												  })
-											  end
-										  end),
-					 awful.button({ }, 4, function ()
-											  awful.client.focus.byidx(1)
-											  if client.focus then client.focus:raise() end
-										  end),
-					 awful.button({ }, 5, function ()
-											  awful.client.focus.byidx(-1)
-											  if client.focus then client.focus:raise() end
-										  end))
+
 
 for s = 1, screen.count() do
 	-- Create a promptbox for each screen
