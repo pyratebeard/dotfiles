4dd988712241b8dd28ef271b3d032e2b1ef7f10e
dudley <pyratebeard@gmail.com>
2016-09-09 12:36:58 +0100

    error handling tidied up, messages changed

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -40,30 +40,27 @@ lain          = require("lain")
 awful.util.spawn_with_shell("xcompmgr -cF &")
 
 
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
+-- error handling
 if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
+  naughty.notify({
+    preset = naughty.config.presets.critical,
+    title = "arrgh! there was mutiny during startup!",
+    text = awesome.startup_errors
+  })
 end
-
--- Handle runtime errors after startup
 do
-    local in_error = false
-    awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
-        if in_error then return end
-        in_error = true
-
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = err })
-        in_error = false
-    end)
+  local in_error = false
+  awesome.connect_signal("debug::error", function (err)
+    if in_error then return end
+    in_error = true
+    naughty.notify({
+      preset = naughty.config.presets.critical,
+      title = "to err is human...",
+      text = err
+    })
+    in_error = false
+  end)
 end
--- }}}
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
