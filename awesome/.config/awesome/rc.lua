d93b858e4695073869aa3ae14444f70b326a6c52
dudley <pyratebeard@gmail.com>
2016-09-09 13:24:03 +0100

    removed main menu, i do not use it

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -104,23 +104,6 @@ for s = 1, screen.count() do
   layouts[1])
 end
 
-		-- {{{ Menu
-		-- Create a laucher widget and a main menu
-		myawesomemenu = {
-		   { "manual", terminal .. " -e man awesome" },
-		   { "edit config", editor_cmd .. " " .. awesome.conffile },
-		   { "restart", awesome.restart },
-		   { "quit", awesome.quit }
-		}
-
-		mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-											{ "open terminal", terminal }
-										  }
-								})
-
-		mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
-											 menu = mymainmenu })
-
 		-- Menubar configuration
 		menubar.utils.terminal = terminal -- Set the terminal for applications that require it
 		-- menubar.set_icon_theme("/usr/share/fonts/HighContrast/index.theme")
@@ -267,7 +250,6 @@ end
 
 		-- {{{ Mouse bindings
 		root.buttons(awful.util.table.join(
-			awful.button({ }, 3, function () mymainmenu:toggle() end),
 			awful.button({ }, 4, awful.tag.viewnext),
 			awful.button({ }, 5, awful.tag.viewprev)
 		))
