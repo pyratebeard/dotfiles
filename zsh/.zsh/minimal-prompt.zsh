86ffcaa276915b08460282abd066c0099b85e734
dudley <root@pyratebeard.net>
2017-07-14 20:15:47 +0100

    removed unused line

--- zsh/.zsh/minimal-prompt.zsh
+++ zsh/.zsh/minimal-prompt.zsh
@@ -11,5 +11,4 @@
 # CODE    http://github.com/pyratebeard/dotfiles
 #
 # Minimal prompt
-#PROMPT='${USER_LEVEL}[%F{white}%1~${USER_LEVEL}]$(prompt_git_info)── -%f '
-PROMPT='%F{cyan}${USERNAME}@%F{white}[$HOSTNAME] : %~# '
+PROMPT='%F{cyan}${USERNAME}@%F{white}[archee] : %~# '
