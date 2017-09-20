8f2a4934e0c7b06c1338529a77bfc36e0ed15ba6
pyratebeard <root@pyratebeard.net>
2017-09-20 13:31:05 +0100

    updated badblood vim and xdefaults. updated boneyard awesome

--- awesome/.config/awesome/themes/boneyard/theme.lua
+++ awesome/.config/awesome/themes/boneyard/theme.lua
@@ -55,13 +55,13 @@ theme.bg_urgent     = "#0b0b0b"
 theme.bg_minimize   = "#0b0b0b"
 
 theme.fg_normal     = "#d9d9d9"
-theme.fg_focus      = "#33aaaa"
+theme.fg_focus      = "#01c8e5"
 theme.fg_urgent     = "#a71c1f"
 theme.fg_minimize   = "#656565"
 
 theme.border_width  = "2"
 theme.border_normal = "#212121"
-theme.border_focus  = "#33aaaa"
+theme.border_focus  = "#01c8e5"
 theme.border_marked = "#ff0056"
 
 -- There are other variable sets
