ad7b326c2fbee8e09bab7faa2eb7b7d021db7a5f
pyratebeard <root@pyratebeard.net>
2019-08-02 22:24:30 +0100

    moved export of GPG_TTY up so it is used

--- zsh/.zsh/keychain.zsh
+++ zsh/.zsh/keychain.zsh
@@ -3,8 +3,9 @@
 # ssh key dir
 SSH_KEY_DIR="$HOME/.ssh"
 
+GPG_TTY=$(tty)
+export GPG_TTY
+
 # funtoo keychain
 eval $(keychain -q --agents ssh,gpg --nogui --eval $SSH_KEY_DIR/*.prv 0xC7877C715113A16D)
 
-GPG_TTY=$(tty)
-export GPG_TTY
