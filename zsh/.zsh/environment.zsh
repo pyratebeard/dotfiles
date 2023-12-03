0a6b747c26f2479f3dba1d9a62db59cca051fb6f
pyratebeard <root@pyratebeard.net>
2023-12-03 23:15:40 +0000

    change locale

--- zsh/.zsh/environment.zsh
+++ zsh/.zsh/environment.zsh
@@ -15,15 +15,15 @@ export VISUAL=vim
 export LYNX_LSS=$HOME/.config/lynx/lynx.lss
 
 # language
-export LC_COLLATE=en_GB.UTF-8
-export LC_CTYPE=en_GB.UTF-8
-export LC_MESSAGES=en_GB.UTF-8
-export LC_MONETARY=en_GB.UTF-8
-export LC_NUMERIC=en_GB.UTF-8
-export LC_TIME=en_GB.UTF-8
-export LC_ALL=en_GB.UTF-8
-export LANG=en_GB.UTF-8
-export LANGUAGE=en_GB.UTF-8
+export LC_COLLATE=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_MESSAGES=en_US.UTF-8
+export LC_MONETARY=en_US.UTF-8
+export LC_NUMERIC=en_US.UTF-8
+export LC_TIME=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
 export LESSCHARSET=utf-8
 
 set -o vi
