31739b989e1e6cddfdd0c9a52d549fc19255d2ec
pyratebeard <root@pyratebeard.net>
2024-05-26 22:01:53 +0100

    fix(zsh): cargo and rust home vars. update path

--- zsh/.config/zsh/environment.zsh
+++ zsh/.config/zsh/environment.zsh
@@ -10,12 +10,14 @@ export XDG_DATA_HOME="$HOME"/.local/share
 export XDG_CACHE_HOME="$HOME"/.local/cache
 export XDG_STATE_HOME="$HOME"/.local/state
 export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:=/tmp}"
-#export ZDOTDIR="$HOME"/.config/zsh
+#export ZDOTDIR="$HOME"/.config/zsh # set in /etc/zsh/zshenv
 export GNUPGHOME="$XDG_DATA_HOME"/gpg
+export CARGO_HOME="$HOME"/.local/lib/cargo
+export RUSTUP_HOME="$HOME"/.local/lib/rustup
 
 # paths
-export PATH=$PATH:$HOME/.local/bin:/usr/local/bin:$HOME/.gem/ruby/2.6.0/bin:$HOME/.cargo/bin
-export GOPATH=$HOME/.local/src/go
+export GOPATH=$HOME/.local/lib/go
+export PATH=$PATH:$HOME/.local/bin:/usr/local/bin:$HOME/.gem/ruby/2.6.0/bin:$CARGO_HOME/bin:$GOPATH:$XDG_DATA_HOME/nvim/mason/bin
 export TF_PLUGIN_CACHE_DIR=$XDG_CACHE_HOME/tf_plugin
 
 # preferred editor for local and remote sessions
